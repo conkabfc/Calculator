2b8ff7ba729906a9dec0137e46c02108ec0abed7
Connor Whitworth <connorwhitworth4@gmail.com>
2023-06-28 11:10:41 +0100

    Added multiple operands

--- index.js
+++ index.js
@@ -5,30 +5,34 @@ console.log('=========================');
 console.log('Please enter the operator:');
 const operator = readline.prompt();
 
-console.log('Please enter your first number:');
-const first_num = parseInt(readline.prompt());
+console.log('How many numbers do you want to ' + operator + '?');
+const operands = parseInt(readline.prompt());
 
-console.log('Please enter your second number:');
-const second_num = parseInt(readline.prompt());
 
+let operandlist = [];
+for (let i = 0; i < operands; i++) {
+    let j = i+1;
+    console.log('Please enter number ' + j + ':');
+    operandlist.push(parseInt(readline.prompt()));
+}
+
+let result = operandlist.shift();
 switch (operator) {
     case "+":
-        console.log('The result:');
-        console.log(first_num + second_num);
+        operandlist.forEach((operand) => result += operand);
         break;
     case "-":
-        console.log('The result:');
-        console.log(first_num - second_num);
+        operandlist.forEach((operand) => result -= operand);
         break;
     case "*":
-        console.log('The result:');
-        console.log(first_num * second_num);
+        operandlist.forEach((operand) => result *= operand);
         break;
     case "/":
-        console.log('The result:');
-        console.log(first_num / second_num);
+        operandlist.forEach((operand) => result /= operand);
         break;
     default:
         console.log('Unrecognised operator');
 }
+console.log('The result:');
+console.log(result);
 
