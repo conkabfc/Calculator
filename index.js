6a2f5dc48d133154c58d41e1e9df49365a3fef76
Connor Whitworth <connorwhitworth4@gmail.com>
2023-06-28 10:42:37 +0100

    Added a switch for operator

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const readline = require('readline-sync');
 console.log('Welcome to the calculator');
+console.log('=========================');
+
+console.log('Please enter the operator:');
+const operator = readline.prompt();
 
 console.log('Please enter your first number:');
 const first_num = parseInt(readline.prompt());
@@ -7,5 +11,24 @@ const first_num = parseInt(readline.prompt());
 console.log('Please enter your second number:');
 const second_num = parseInt(readline.prompt());
 
-console.log('The result:');
-console.log(first_num * second_num);
+switch (operator) {
+    case "+":
+        console.log('The result:');
+        console.log(first_num + second_num);
+        break;
+    case "-":
+        console.log('The result:');
+        console.log(first_num - second_num);
+        break;
+    case "*":
+        console.log('The result:');
+        console.log(first_num * second_num);
+        break;
+    case "/":
+        console.log('The result:');
+        console.log(first_num / second_num);
+        break;
+    default:
+        console.log('Unrecognised operator');
+}
+
